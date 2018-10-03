7693632163798b05b0f9b7b9adc09bfcf020fc3e
Pradeesh112 <Pradeesh112@protonmail.com>
2018-10-03 22:31:43 +0530

    first commit

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -887,7 +887,7 @@ c.downloads.remove_finished =10000
 
 ## Characters used for hint strings.
 ## Type: UniqueCharString
-# c.hints.chars = 'asdfghjkl'
+#c.hints.chars = 'asdfghjkl'
 
 ## Dictionary file to be used by the word hints.
 ## Type: File
@@ -898,7 +898,7 @@ c.downloads.remove_finished =10000
 ## Valid values:
 ##   - javascript: Better but slower
 ##   - python: Slightly worse but faster
-# c.hints.find_implementation = 'python'
+c.hints.find_implementation = 'python'
 
 ## Hide unmatched hints in rapid mode.
 ## Type: Bool
@@ -906,7 +906,7 @@ c.hints.hide_unmatched_rapid_hints = True
 
 ## Minimum number of characters used for hint strings.
 ## Type: Int
-# c.hints.min_chars = 1
+#c.hints.min_chars = 1
 
 ## Mode to use for hints.
 ## Type: String
@@ -914,7 +914,7 @@ c.hints.hide_unmatched_rapid_hints = True
 ##   - number: Use numeric hints. (In this mode you can also type letters from the hinted element to filter and reduce the number of elements that are hinted.)
 ##   - letter: Use the characters in the `hints.chars` setting.
 ##   - word: Use hints words based on the html elements and the extra words.
-# c.hints.mode = 'letter'
+#c.hints.mode = 'letter'
 
 ## Comma-separated list of regular expressions to use for 'next' links.
 ## Type: List of Regex
@@ -1644,7 +1644,7 @@ config.bind('<Return>', 'follow-hint', mode='hint')
 
 ## Bindings for insert mode
 config.bind('<Ctrl-E>', 'open-editor', mode='insert')
-config.bind('<Escape>', 'leave-mode', mode='insert')
+config.bind('aa', 'leave-mode', mode='insert')
 config.bind('<Shift-Ins>', 'insert-text {primary}', mode='insert')
 
 ## Bindings for passthrough mode
